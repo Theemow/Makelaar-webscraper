f3649285c872e9a899724f575e9497aa086f3a42
Theemow <51474937+Theemow@users.noreply.github.com>
2025-07-14 18:26:29 +0200

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -5,8 +5,11 @@ HuurhuisWebscraper package for scraping rental property listings.
 from scrapers.base_scraper import BaseScraper
 from scrapers.ditters_scraper import DittersScraper
 from scrapers.interhouse_scraper import InterHouseScraper
+from scrapers.nederwoon_scraper import NederwoonScraper
 from scrapers.pararius_scraper import ParariusScraper
 from scrapers.scraper_factory import ScraperFactory
+from scrapers.vastgoednederland_scraper import VastgoedNederlandScraper
+from scrapers.vbt_scraper import VBTScraper
 from scrapers.vdbunt_scraper import VdBuntScraper
 from scrapers.zonnenberg_scraper import ZonnenbergScraper
 
@@ -17,5 +20,8 @@ __all__ = [
     "ZonnenbergScraper",
     "DittersScraper",
     "InterHouseScraper",
+    "NederwoonScraper",
+    "VastgoedNederlandScraper",
+    "VBTScraper",
     "ScraperFactory",
 ]
